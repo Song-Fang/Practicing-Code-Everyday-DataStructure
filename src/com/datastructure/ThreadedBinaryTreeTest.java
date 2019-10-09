8619ac886cc0c2bb4994f91de17161ced7ca2655
hitfangsong <sof10@pitt.edu>
2019-10-08 23:21:40 -0400

    Signed-off-by: hitfangsong <sof10@pitt.edu>

--- src/com/datastructure/ThreadedBinaryTreeTest.java
+++ src/com/datastructure/ThreadedBinaryTreeTest.java
@@ -9,5 +9,51 @@ class ThreadedBinaryTree{
 }
 
 class ThreadedTreeNode{
+	private int id;
+	ThreadedTreeNode left;
+	ThreadedTreeNode right;
+	
+	int leftNodeType;
+	int rightNodeType;
+	
+	public int getId() {
+		return id;
+	}
+	public void setId(int id) {
+		this.id = id;
+	}
+	public ThreadedTreeNode getLeft() {
+		return left;
+	}
+	public void setLeft(ThreadedTreeNode left) {
+		this.left = left;
+	}
+	public ThreadedTreeNode getRight() {
+		return right;
+	}
+	public void setRight(ThreadedTreeNode right) {
+		this.right = right;
+	}
+	public int getLeftNodeType() {
+		return leftNodeType;
+	}
+	public void setLeftNodeType(int leftNodeType) {
+		this.leftNodeType = leftNodeType;
+	}
+	public int getRightNodeType() {
+		return rightNodeType;
+	}
+	public void setRightNodeType(int rightNodeType) {
+		this.rightNodeType = rightNodeType;
+	}
+	
+	@Override
+	public String toString() {
+		return "ThreadedTreeNode [id=" + id + "]";
+	}
+	
+	
+	
+	
 	
 }
