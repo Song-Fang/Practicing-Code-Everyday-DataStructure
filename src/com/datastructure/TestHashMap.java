6ebaf2f1d7bbdab18d1ee6cad48107f92b97f402
hitfangsong <sof10@pitt.edu>
2019-10-12 10:28:11 -0400

    Signed-off-by: hitfangsong <sof10@pitt.edu>

---
+++ src/com/datastructure/TestHashMap.java
@@ -0,0 +1,14 @@
+package com.datastructure;
+
+import java.util.HashMap;
+import java.util.Hashtable;
+import java.util.Map;
+
+public class TestHashMap {
+public static void main(String [] args) {
+	Map ht = new HashMap();
+	ht.put(null, null);
+	
+	System.out.println(ht);
+}
+}
