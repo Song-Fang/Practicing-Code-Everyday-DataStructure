500eea5875769fc3b42c90e6b3244f13f716b410
hitfangsong <sof10@pitt.edu>
2019-10-12 13:02:59 -0400

    Signed-off-by: hitfangsong <sof10@pitt.edu>

--- src/com/datastructure/TreeMapTest.java
+++ src/com/datastructure/TreeMapTest.java
@@ -1,5 +1,27 @@
 package com.datastructure;
 
+import java.util.Iterator;
+import java.util.Map.Entry;
+import java.util.Set;
+import java.util.TreeMap;
+
+import org.junit.Test;
+
 public class TreeMapTest {
 
+	@Test
+	public void treeMapTest() {
+		TreeMap<String,Integer> treeMap = new TreeMap<String,Integer>();
+		treeMap.put("Francis", 23);
+		treeMap.put("Dogus", 25);
+		treeMap.put("Lee", 27);
+		treeMap.put("Lynn", 29);
+		treeMap.put("Peter", 33);
+		
+		Set<Entry<String,Integer>> entrySet = treeMap.entrySet();
+		Iterator<Entry<String, Integer>> iterator = entrySet.iterator();
+		while(iterator.hasNext()) {
+			System.out.println(iterator.next());
+		}
+	}
 }
